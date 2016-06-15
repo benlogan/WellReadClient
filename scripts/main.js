5956eea0c51e72be2da7015daf44a3efec0ac38d
Ben Logan <benlogan1981@gmail.com>
2016-06-15 21:57:48 +0100

    various changes!

--- scripts/main.js
+++ scripts/main.js
@@ -110,7 +110,7 @@ function getBookDetails(isbn) {
 
                     //var html = createSummaryTableRow(val.text);
                     //$('#summaryTable').append(html);
-                    summaryRowsHtml += createSummaryTableRow(val.id, val.text, val.votes);
+                    summaryRowsHtml += createSummaryTableRow(val.id, val.datetime, val.text, val.name, val.votes);
                 });
                 $('#summaryTable').html(summaryRowsHtml);
             }
@@ -141,9 +141,14 @@ function getTopBooks(number) {
     });
 }
 
-function createSummaryTableRow(summaryID, summaryText, votes) {
+// actually two rows! one for the author name
+function createSummaryTableRow(summaryID, summaryTime, summaryText, summaryAuthor, votes) {
     //<input type="hidden" name="_id_" value="34547563">
-    return "<tr><td class='votecell'><div class='vote'><input type='hidden' name='_id_' value=" + summaryID + "><a id='voteUp' class='vote-up-off'></a><span itemprop='upvoteCount' class='vote-count-post '>" + votes + "</span><a id='voteDown' class='vote-down-off'></a></div></td><td class='postcell'>" + summaryText + "</td></tr>";
+    if(summaryTime) {
+        summaryTime = new Date(summaryTime).toLocaleString('en-GB', { hour12: false });
+    }
+    return "<tr><td class='votecell'><div class='vote'><input type='hidden' name='_id_' value=" + summaryID + "><a id='voteUp' class='vote-up-off'></a><span itemprop='upvoteCount' class='vote-count-post '>" + votes + "</span><a id='voteDown' class='vote-down-off'></a></div></td><td class='postcell'>" + summaryText + "</td><td class='summaryAuthor'>" + summaryAuthor + " " + summaryTime + "</td></tr>";
+    //<tr class='summaryAuthorRow'><td></td><td class='summaryAuthor'>" + summaryAuthor + " (" + summaryTime + ")</td></tr>"; // used to be a seperate row, but this complicates sorting!
     // <a class='star-off'></a> disabled for now, no need for it - might eventually become 'my favourites'
 }
 
@@ -156,16 +161,25 @@ $(document).on("click", '#summaryTable', function(e) {
     var summaryID = e.target.parentNode.children[0].value;
     var vote; // 1 = up, -1 = down
     if(e.target.id == "voteUp") {
-        //alert("Vote Up! ID : " + summaryID);
+        //console.log("Vote Up! ID : " + summaryID);
         vote = 1;
         // e.target.previousSibling.value
         // slighty hacky way of getting hold of the hidden input value (ID)
     } else if(e.target.id == "voteDown") {
-        //alert("Vote Down! ID : " + summaryID);
+        //console.log("Vote Down! ID : " + summaryID);
         vote = -1;
     }
-    // FIXME apply the vote, via a new POST call
-    if(vote == 1 || vote == -1) {
+    if((vote == 1 || vote == -1) && !sessionStorage.getItem(summaryID)) {
+        sessionStorage.setItem(summaryID, vote);
+        
+        // don't wait for server and page refresh - update the numbers on client
+        var voteCount = e.target.parentNode.getElementsByClassName('vote-count-post').item(0);
+        //var voteCount = e.target.parentNode.getElementById('voteCount');
+        var newVoteCount = +voteCount.textContent + vote;
+        voteCount.textContent = newVoteCount;
+        // this works well, but doesn't currently handle reordering the list!
+        sortTable('summaryTable');
+        
         $.ajax({
             url: 'http://127.0.0.1:1337/voteSummary/',
             //url: 'http://wellreadserver.herokuapp.com',
@@ -175,12 +189,36 @@ $(document).on("click", '#summaryTable', function(e) {
                 // we don't end up in here, no response!
             },
             complete: function(data) {
-                refreshPage();
+                console.log("Vote POST complete - refreshing page!");
+                //refreshPage();
             }
         });
+    } else if(sessionStorage.getItem(summaryID)) {
+        alert('You\'ve already voted on this item!');
     }
-    // FIXME you can only vote once! (could just implement this on the server side for now)
 });
+
+// http://stackoverflow.com/questions/7558182/sort-a-table-fast-by-its-first-column-with-javascript-or-jquery
+function sortTable(tableElementId) {
+    var tbl = document.getElementById(tableElementId).tBodies[0];
+    var store = [];
+    for(var i=0, len=tbl.rows.length; i<len; i++) {
+        var row = tbl.rows[i];
+        
+        var sortnr = parseFloat(row.cells[0].getElementsByClassName('vote-count-post').item(0).textContent);
+        //var sortnr = parseFloat(row.cells[0].textContent || row.cells[0].innerText);
+        
+        if(!isNaN(sortnr)) store.push([sortnr, row]);
+    }
+    store.sort(function(x,y) {
+        return y[0] - x[0]; // we want descending...
+        //return x[0] - y[0];
+    });
+    for(var i=0, len=store.length; i<len; i++) {
+        tbl.appendChild(store[i][1]);
+    }
+    store = null;
+}
 
 /*
 function writeSummaryInput() {
@@ -207,6 +245,13 @@ function refreshPage() {
 // new way of writing/posting summaries
 $(document).on('submit', '#SummaryText', function(e) {
     var summary = $('#SummaryTextArea').val(); // use jquery to fetch value
+    
+    if(!summary) {
+        alert('Please enter some text...');
+        e.preventDefault(); // don't navigate away!
+        return;
+    }
+    
     console.log("About to POST summary : " + summary + " for user : " + oAuthID_memory);
     $.ajax({
         url: 'http://127.0.0.1:1337/writeSummary/',
